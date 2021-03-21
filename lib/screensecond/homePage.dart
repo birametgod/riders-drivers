12df14ecb6feecc74119e34e800cd06152a240e0
SENE Birame <birametgod95@gmail.com>
2021-03-21 19:43:00 +0100

    BIRAME: prepare to go develop

--- lib/screensecond/homePage.dart
+++ lib/screensecond/homePage.dart
@@ -1,8 +1,8 @@
-import 'package:flutter/material.dart';
-import 'ride_search.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'ride_info.dart';
+import 'package:flutter/material.dart';
+
 import 'ride_accept.dart';
+import 'ride_info.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,8 +16,7 @@ void main() async {
 
       // Handle '/details/:id'
       var uri = Uri.parse(settings.name);
-      if (uri.pathSegments.length == 2 &&
-          uri.pathSegments.first == 'details') {
+      if (uri.pathSegments.length == 2 && uri.pathSegments.first == 'details') {
         var id = uri.pathSegments[1];
         return MaterialPageRoute(builder: (context) => DetailScreen(id: id));
       }
@@ -27,15 +26,12 @@ void main() async {
   ));
 }
 
-
 //add alert message with notifications https://itnext.io/local-notifications-in-flutter-6136235e1b51
 //https://www.freecodecamp.org/news/how-to-add-push-notifications-to-flutter-app/
 //https://medium.com/firebase-tips-tricks/how-to-use-cloud-firestore-in-flutter-9ea80593ca40
 
 //https://medium.com/firebase-tips-tricks/how-to-use-firebase-realtime-database-with-flutter-ebd98aba2c91 android firebase config
 //https://stackoverflow.com/questions/49351648/how-do-i-disable-a-button-in-flutter
-
-
 
 //https://medium.com/flutter-community/flutter-creating-a-route-calculator-using-google-maps-71699dd96fb9
 //https://github.com/sbis04/flutter_maps/tree/master/lib
